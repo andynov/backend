4d7dbbfb18d59efe51544d8f003b47ff261fba74
andres novio <andresnov@hotmail.com>
2023-12-20 17:00:53 -0300

    desafío 3

---
+++ Proyecto/src/ProductManager.js
@@ -0,0 +1,109 @@
+const fs = require("fs").promises;
+
+class ProductManager {
+    static lastId = 0;
+    
+    constructor(path){
+        this.products = [];
+        this.path = path;
+    }
+    
+    async addProduct(newObject) {
+        let {title, description, price, thumbnail, code, stock} = newObject;
+
+        if(!title || !description || !price || !thumbnail || !code || !stock){
+            console.log("Completa todos los campos, por favor.");
+            return;
+        }
+
+        if(this.products.some(item => item.code === code)){
+            console.log("El código se repite. Intenta generar otro código, por favor.");
+            return;
+        }
+
+        const newProduct = {
+            id: ++ProductManager.lastId,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        this.products.push(newProduct);
+
+        await this.saveProduct(this.products);
+    }
+
+    getProducts(){
+        console.log(this.products);
+    }
+
+    async getProductById(id){
+        try{
+            const arrayProducts = await this.readProduct();
+            const searched = arrayProducts.find(item => item.id === id);
+            if (!searched) {
+                console.log("No hemos encontrado el producto.")
+            } else {
+                console.log("Hemos encontrado el producto");
+                return searched;
+            }
+
+        } catch(error) {
+            console.log("Error al leer el archivo ", error)
+        }
+    }
+
+    async readProduct() {
+        try {
+            const resp = await fs.readFile(this.path, "utf-8");
+            const arrayProducts = JSON.parse(resp);
+            return arrayProducts;
+        } catch (error) {
+            console.log("Error al leer el archivo ", error)
+        }
+    }
+    
+    async saveProduct(arrayProducts) {
+            try {
+                await fs.writeFile(this.path, JSON.stringify(arrayProducts, null, 2))
+            } catch (error){
+                console.log("Error al guardar el archivo ", error)
+            }
+    }
+
+    async updateProduct(id, productUpdated) {
+        try{
+            const arrayProducts = await this.readProduct();
+            const index = arrayProducts.findIndex(item => item.id === id);
+            if (index !== -1) {
+                arrayProducts.splice(index, 1, productUpdated);
+                await this.saveProduct(arrayProducts);
+            } else{
+                console.log("No se encontró el producto");
+            }
+        } catch(error){
+            console.log("Error al actualizar el producto", error);
+        }
+    }
+
+    async deleteProduct(id){
+        try{
+            const arrayProducts = await this.readProduct();
+            const index = arrayProducts.findIndex(item => item.id === id);
+            if (index !== -1) {
+                arrayProducts.splice(index, 1);
+                await this.saveProduct(arrayProducts);
+            } else{
+                console.log("No se encontró el producto");
+            }
+        } catch(error){
+            console.log("Error al borrar el producto", error);
+        }
+    }
+}
+
+module.exports = ProductManager;
+
---
+++ Proyecto/src/app.js
@@ -0,0 +1,59 @@
+const ProductManager = require("./ProductManager")
+
+const manager = new ProductManager("./products.json")
+
+const express = require("express");
+
+const app = express();
+
+const PUERTO = 8080;
+
+app.get("/", (req, res) =>{
+    res.send(products)
+})
+
+
+// Limitar el número de elementos del array y mostrarlo:
+
+app.get("/products", async (req, res) => {
+    try{
+        const arrayProducts = await manager.readProduct();
+        let limit = parseInt(req.query.limit);
+
+        if (limit) {
+            const arrayLimited = arrayProducts.slice(0, limit);
+            return res.send(arrayLimited)
+        } else{
+            return res.send(arrayProducts);
+        }
+
+    } catch(error) {
+        console.log(error);
+        return res.send("Error al procesar tu pedido")
+    }
+})
+
+// Retornar un elemento del array por ID:
+
+app.get("/products/:pid", async (req, res) => {
+    try{
+        let pid = parseInt(req.params.pid)
+        const searched = await manager.getProductById(pid);
+
+        if (searched) {
+            return res.send(searched);
+        } else{
+            return res.send("ID de Producto incorrecto");
+        }
+
+    } catch(error){
+        console.log(error);
+        return res.send("Error al retornar el elemento")
+    }
+})
+
+app.listen(PUERTO, () =>{
+    console.log(`Escuchando en http://localhost:${PUERTO}`)
+})
+
+
